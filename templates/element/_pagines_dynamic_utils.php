1039cd81b62f592d43c5b9898a38574f777d8b7e
arnaubarcelona <37509047+arnaubarcelona@users.noreply.github.com>
2026-02-27 12:44:19 +0100

    Add dynamic page elements for configs and years data

---
+++ templates/element/_pagines_dynamic_utils.php
@@ -0,0 +1,86 @@
+<?php
+declare(strict_types=1);
+
+use Cake\ORM\TableRegistry;
+
+if (!function_exists('paginesGetConfigValue')) {
+    function paginesGetConfigValue(string $configName): string
+    {
+        $configsTable = TableRegistry::getTableLocator()->get('Configs');
+        $config = $configsTable->find()
+            ->select(['valuetext'])
+            ->where(['name' => $configName])
+            ->first();
+
+        return trim((string)($config->valuetext ?? ''));
+    }
+}
+
+if (!function_exists('paginesGetYearMaxDate')) {
+    function paginesGetYearMaxDate(string $field): ?\DateTimeInterface
+    {
+        $yearsTable = TableRegistry::getTableLocator()->get('Years');
+        $year = $yearsTable->find()
+            ->select([$field])
+            ->where(["{$field} IS NOT" => null])
+            ->order([$field => 'DESC'])
+            ->first();
+
+        $date = $year?->{$field};
+        return $date instanceof \DateTimeInterface ? $date : null;
+    }
+}
+
+if (!function_exists('paginesGetLatestYearFieldValue')) {
+    function paginesGetLatestYearFieldValue(string $field): string
+    {
+        $yearsTable = TableRegistry::getTableLocator()->get('Years');
+        $year = $yearsTable->find()
+            ->select([$field])
+            ->where(["{$field} IS NOT" => null])
+            ->order(['id' => 'DESC'])
+            ->first();
+
+        return trim((string)($year?->{$field} ?? ''));
+    }
+}
+
+if (!function_exists('paginesFormatCatalanDate')) {
+    function paginesFormatCatalanDate(?\DateTimeInterface $date): string
+    {
+        if (!$date) {
+            return '';
+        }
+
+        $weekDays = [
+            0 => 'diumenge',
+            1 => 'dilluns',
+            2 => 'dimarts',
+            3 => 'dimecres',
+            4 => 'dijous',
+            5 => 'divendres',
+            6 => 'dissabte',
+        ];
+
+        $months = [
+            1 => 'gener',
+            2 => 'febrer',
+            3 => 'març',
+            4 => 'abril',
+            5 => 'maig',
+            6 => 'juny',
+            7 => 'juliol',
+            8 => 'agost',
+            9 => 'setembre',
+            10 => 'octubre',
+            11 => 'novembre',
+            12 => 'desembre',
+        ];
+
+        $weekday = $weekDays[(int)$date->format('w')] ?? '';
+        $day = (int)$date->format('j');
+        $month = $months[(int)$date->format('n')] ?? '';
+
+        return trim(sprintf('%s %d de %s', $weekday, $day, $month));
+    }
+}
